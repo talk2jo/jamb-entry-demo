00a44178a6d75bebeb5686c616e23cec78c712cc
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-02-23 18:01:20 +0100

    implement search function for search and search option

--- dashboard/public/js/search.js
+++ dashboard/public/js/search.js
@@ -1,6 +1,20 @@
 const mySearch = () => {
+  let index = 0;
+  let e = document.getElementById("searchOption");
+  let selectedValue = e.options[e.selectedIndex].text;
+
+  if (selectedValue == "Reg-no") {
+    index = 1;
+  } else if (selectedValue == "Year") {
+    index = 5;
+  } else if (selectedValue == "University") {
+    index = 6;
+  }
+
+  console.log(selectedValue);
+
   // Declare variables 
-  let input, filter, table, tr, td, i, txtValue;
+  let input, filter, tr, td, i, txtValue;
   input = document.getElementById("mySearch");
   filter = input.value.toUpperCase();
   table = document.getElementById("tbody");
@@ -8,7 +22,7 @@ const mySearch = () => {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[3];
+    td = tr[i].getElementsByTagName("td")[index];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
